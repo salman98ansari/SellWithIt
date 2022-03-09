c939e9188eaefcf84623c540dbc29773c235fffe
Salman Ansari <salman98101@gmail.com>
2022-03-09 18:47:29 +0530

    changes

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import jwtDecode from "jwt-decode";
+import { AppLoading } from "expo";
 
 import OfflineNotice from "./app/components/OfflineNotice";
 import AuthNavigator from "./app/navigation/AuthNavigator";
@@ -11,6 +12,7 @@ import authStorage from "./app/auth/storage";
 
 export default function App() {
   const [user, setUser] = useState();
+  const [isReady, setIsReady] = useState(false);
 
   const restoreToken = async () => {
     const token = await authStorage.getToken();
@@ -21,6 +23,12 @@ export default function App() {
   useEffect(() => {
     restoreToken();
   }, []);
+
+  // if (!isReady) {
+  //   return (
+  //     <AppLoading startAsync={restoreToken} onFinish={() => setIsReady(true)} />
+  //   );
+  // }
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
